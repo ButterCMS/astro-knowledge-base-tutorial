ed3efbdeed5f4b2bb185fb40ec1e7fb50b025788
miracleonyenma <miracleficient@gmail.com>
2023-03-23 00:00:10 +0100

    feat: add @astrojs/netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 // ./astro.config.mjs
 
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: "server"
+  output: "server",
+  adapter: netlify()
 });
